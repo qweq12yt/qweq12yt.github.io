be674b657c61771452818acd92f2aff6a1ecab1d
qweq12yt <45515587+qweq12yt@users.noreply.github.com>
2021-01-05 23:20:44 -0300

    testing mine coin

--- scripts/game_test.js
+++ scripts/game_test.js
@@ -1,3 +1,10 @@
 function updateTime() {
-    document.getElementById("time").innerHTML = Date.now()
+    document.getElementById("time").innerHTML = Date.now();
+}
+
+var coin = 0;
+
+function mineCoin() {
+    coin++
+    document.getElementById("coin").innerHTML = coin;
 }
