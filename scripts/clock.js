515c704a6117c0ce778a178e22f9e920355560fa
qweq12yt <45515587+qweq12yt@users.noreply.github.com>
2021-01-06 00:47:16 -0300

    giving up on imports, one file it is

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,4 +1,11 @@
-import updateClock from "game_test.js";
-
 const GLOBAL_TIMING = 250;
 var clock = setInterval(updateClock(), GLOBAL_TIMING);
+
+function updateClock(){
+    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+    coins++;
+    document.getElementById("coins").innerHTML = coins;
+}
+
+var d = new Date();
+var coins = 0;
