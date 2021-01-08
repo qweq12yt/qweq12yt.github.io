da71b59ad09fccf810c7700aa56d7c210b854b51
qweq12yt <45515587+qweq12yt@users.noreply.github.com>
2021-01-08 15:29:51 -0300

    under_construction_v0.0.1

---
+++ scripts/beta_game.js
@@ -0,0 +1,58 @@
+const GLOBAL_TIMING = 100;
+var clock = setInterval(updateAll, GLOBAL_TIMING);
+
+// Updates all timed logic
+function updateAll() {
+    for(key in logic){
+        logic[key]();
+    }
+    for(key in draw){
+        draw[key]();
+    }
+}
+
+// These functions run every GLOBAL_TIMING miliseconds, logic updates
+var logic = {
+    accumulateCoins : function(){
+        vars.coins += vars.rate;
+    },
+    blinkCursor : function(){
+        cursor_hidden = ! cursor_hidden;
+    },
+    modifyScreenState : function() {
+        if (vars.cursor_hidden) {
+            graphics.screen_state = ">>> WAITING FOR INPUT ";
+        }
+        else {
+            graphics.screen_state = ">>> WAITING FOR INPUT _";
+        }
+    }
+
+}
+
+// These functions run every GLOBAL_TIMING miliseconds, drawing updates
+var draw = {
+    drawScreen : function(){
+        document.getElementById("main-screen").innerHTML = graphics.screen_state;
+    }
+}
+
+// Variable dictionary
+var vars = {
+    coins : 0,
+    rate : 0.01,
+    cursor_hidden : false
+}
+
+// ascii graphics dictionary
+var graphics = {
+    screen_state : ">>> WAITING FOR INPUT _"
+}
+
+// Here beggins all functions directly used in user inputs
+function addRate() {
+    vars.rate += 0.01;
+}
+
+
+// this seems rather cumbersome and silly taking in account how classes work... i don't care though
